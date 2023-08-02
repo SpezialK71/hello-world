315d146d1d04d6c00760638452952358baa7bbe2
Kurt <130383415+SpezialK71@users.noreply.github.com>
2023-08-02 18:38:34 -0500

    Create helloworld.cbl
    hello world in COBOL

---
+++ helloworld.cbl
@@ -0,0 +1,15 @@
+       identification division.
+       program-id. helloworld.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+           display 'Hello COBOL World!'.
+
+           goback.
+
+       end program helloworld.
